9ff66edd9a3dc0c1288fb36dbe523331d8f205b8
L-Antoine <34649108+L-Antoine@users.noreply.github.com>
2018-03-21 00:19:05 +0100

    creation icon + dimensions de mon tel + activite joueur menu + test dynamic layout

--- app/src/main/java/com/example/senalalumiere/petit_bac/MainActivity.java
+++ app/src/main/java/com/example/senalalumiere/petit_bac/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.senalalumiere.petit_bac;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends Activity {
 
@@ -9,5 +11,9 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void jActivity (View view){
+        startActivity(new Intent(this, com.example.senalalumiere.petit_bac.classes.JoueurActivity.class));
     }
 }
